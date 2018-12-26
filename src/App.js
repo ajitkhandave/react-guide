3f46ad5764618017867ed0e7f6c1ac6513f01390
Ajit <akhandave@shutterfly.com>
2018-12-26 23:07:22 +0530

    adding person function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <h1>Hi, I am react App</h1>
+        <h1>Hi, I am react App</h1>
+        <Person />
       </div>
     );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Is react works'))
   }
 }
 
